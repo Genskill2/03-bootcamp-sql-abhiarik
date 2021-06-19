c990548b1a53936b570df75486a229bd3b0b85c3
Abhi <jaswanthabhi1111@gmail.com>
2021-06-20 00:02:45 +0530

    3

--- create.sql
+++ create.sql
@@ -1,9 +1,9 @@
-CREATE TABLE publisher(id int primary key AUTOINCREMENT, name text, country text);
+CREATE TABLE publisher(id integer primary key AUTOINCREMENT, name text, country text);
 
-CREATE TABLE books(id integer primary key AUTOINCREMENT, title text, publisher int,
+CREATE TABLE books(id integer primary key AUTOINCREMENT, title text, publisher integer,
 foreign key(publisher) references publisher(id));
 
-CREATE TABLE subjects(id int AUTOINCREMENT, name text, primary key(id, name));
+CREATE TABLE subjects(id integer AUTOINCREMENT, name text, primary key(id, name));
 
-CREATE TABLE books_subjects(book int, subject int, foreign key (book) references books(id),
+CREATE TABLE books_subjects(book integer, subject integer, foreign key (book) references books(id),
 foreign key(subject) references subjects(id));
