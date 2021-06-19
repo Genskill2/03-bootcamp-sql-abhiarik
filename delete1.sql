4bffa4266b1d80dfbbc7794da179b29241db55b5
Abhi <jaswanthabhi1111@gmail.com>
2021-06-20 03:54:16 +0530

    final

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-DELETE from books_subjects where subject in (SELECT id from subjects where name='History' );
+DELETE from books_subjects where name='History';
 DELETE from subjects where name='History';
