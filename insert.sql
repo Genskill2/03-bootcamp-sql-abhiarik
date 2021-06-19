e78c6e17e9f282d0a9b5dde981f266ff217c6a39
Abhi <jaswanthabhi1111@gmail.com>
2021-06-20 00:31:13 +0530

    02

--- insert.sql
+++ insert.sql
@@ -1,41 +1,41 @@
 INSERT INTO publisher(name, country)
- values (PHI,India),
-        (Harper,USA),
-        (GCP,USA),
-        (Avery,USA),
-        ('Del Rey',UK),
-        (Vintage,UK);
+ values ('PHI','India'),
+        ('Harper','USA'),
+        ('GCP','USA'),
+        ('Avery','USA'),
+        ('Del Rey','UK'),
+        ('Vintage','UK');
 
 INSERT INTO books(title, publisher) 
-values  (The C Programming Language,PHI),
-        (The Go Programming Language,PHI),
-        (The UNIX Programming Language,PHI),
-        (Cryptonomicon, Harper),
-        (Deep Work,GCP),
-        (Atomic Habits,Avery),
-        (The City and the City,'Del Rey'),
-        (THE Great War for Civilisation,Vintage);
+values  ('The C Programming Language','PHI'),
+        ('The Go Programming Language','PHI'),
+        ('The UNIX Programming Language','PHI'),
+        ('Cryptonomicon', 'Harper'),
+        ('Deep Work','GCP'),
+        ('Atomic Habits','Avery'),
+        ('The City and the City','Del Rey'),
+        ('THE Great War for Civilisation','Vintage');
 
 
 
 INSERT INTO subjects(id, name) 
-values  (1,C),
-        (1,UNIX),
-        (1,Technology),
-        (2,GO),
-        (2,Technology),
-        (3,UNIX),
-        (3,Technology),
-        (4,Technology),
-        (4,Science Fiction),
-        (5,Technology),
-        (5,Productivity),
-        (6,Technology),
-        (6,Psychology),
-        (7,Science Fiction),
-        (7,Politics),
-        (8,Politics),
-        (8,History);
+values  (1,'C'),
+        (1,'UNIX'),
+        (1,'Technology'),
+        (2,'GO'),
+        (2,'Technology'),
+        (3,'UNIX'),
+        (3,'Technology'),
+        (4,'Technology'),
+        (4,'Science Fiction'),
+        (5,'Technology'),
+        (5,'Productivity'),
+        (6,'Technology'),
+        (6,'Psychology'),
+        (7,'Science Fiction'),
+        (7,'Politics'),
+        (8,'Politics'),
+        (8,'History');
 
 
 
