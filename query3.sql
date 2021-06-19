9d28b4ec9f0744d627df8dbd6f60946f49e9302d
Abhi <jaswanthabhi1111@gmail.com>
2021-06-20 03:35:15 +0530

    q13

--- query3.sql
+++ query3.sql
@@ -1,2 +1,2 @@
-SELECT books.title from books inner join books_subjects on books_subjects.book=books.id where books_subjects.subject,books_subjects.name in (SELECT subjects.id,subjects.name from subjects inner join books_subjects on books_subjects.subject,books_subjects.name=subjects.id,subjects.name where subjects.name in ('Technology','Politics'););
+SELECT books.title from books inner join books_subjects on books_subjects.book=books.id where books_subjects.name in ('Technology','Politics');
 
